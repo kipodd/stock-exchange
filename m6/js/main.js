34ff5a1565d293ad297fab1e6f2187086ef2726c
kipodd <17299316+kipodd@users.noreply.github.com>
2020-03-21 16:14:01 +0200

    milestone 6

---
+++ m6/js/main.js
@@ -0,0 +1,16 @@
+function displayMarquee(){
+    const MyMarquee = new Marquee(document.getElementById(`stockMarqueeWrapper`));
+    MyMarquee.getRealTimePrices();
+}
+
+function displayContent() {
+    const searchButton = document.getElementById("searchButton");
+    const searchInput = document.getElementById("searchInput");
+    const resultsList = document.getElementById("resultsList");
+    const Form = new SearchForm(searchButton, searchInput, resultsList);
+
+    Form.userInputListen();
+}
+
+displayMarquee();
+displayContent();
---
+++ m6/js/search-form.js
@@ -0,0 +1,31 @@
+class SearchForm {
+    constructor(searchButton, searchInput, resultsList) {
+        this.searchButton = searchButton;
+        this.searchInput = searchInput;
+        this.resultsList = resultsList;
+    }
+
+    debounce(func, delay) {
+        let timeoutID;
+
+        if (timeoutID) {
+            clearTimeout(timeoutID);
+        }
+
+        return function (...args) {
+            timeoutID = setTimeout(() => {
+                func(...args);
+            }, delay);
+        }
+    }
+
+    userInputListen() {
+        document.getElementById("searchButton").addEventListener("click", this.getSearchInput);
+        // document.getElementById("searchInput").addEventListener("keyup", this.debounce(this.getSearchInput, 1000));
+    }
+
+    getSearchInput() {
+        this.ResultsClass = new SearchResults(resultsList);
+        this.ResultsClass.getResults(searchInput.value);
+    }
+}
