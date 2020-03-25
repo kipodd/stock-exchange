271d0214a82ffaa0a5fcfe6e174169e325279dd2
kipodd <17299316+kipodd@users.noreply.github.com>
2020-03-25 23:36:53 +0200

    m11 removed commented out code

--- m11/js/company.js
+++ m11/js/company.js
@@ -5,6 +5,7 @@ function getSymbolsQueryString() {
 
 function createCompanyContainers(companyContainerWrapper, symbols) {
     const columnsAmount =  12 / symbols.length;
+
     for (const symbol of symbols) {
         companyContainerWrapper.insertAdjacentHTML(`beforeend`, `
             <div id="${symbol}Container" class="col-${columnsAmount}"></div>
